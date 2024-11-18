0afca816378920b9022a515b110ca93fc969c9d1
webhostdevs <webhostdev59@gmail.com>
2024-11-19 04:52:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgr(),
+    svgr()
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),  // Adjust 'src' if your source folder is elsewhere
-    },
-  },
+  
 });
