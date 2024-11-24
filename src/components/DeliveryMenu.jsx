c2e30c27ff492ce231ce8a17305f143806e31eef
webhostdevs <webhostdev59@gmail.com>
2024-11-24 21:17:48 +0530

    Update DeliveryMenu.jsx

--- src/components/DeliveryMenu.jsx
+++ src/components/DeliveryMenu.jsx
@@ -289,7 +289,7 @@ const DeliveryMenu = () => {
         <form onSubmit={handleGuestCountSubmit} className="flex flex-col gap-4">
           <input
             type="number"
-            min="1"
+            min="10"
             value={guestCount}
             onChange={(e) => setGuestCount(e.target.value)}
             className="border rounded px-4 py-2"
@@ -315,13 +315,13 @@ const DeliveryMenu = () => {
         <div className="flex gap-4">
           <button 
             onClick={() => handleMenuTypeSelect('veg')}
-            className="bg-blue-500 text-white px-8 py-4 rounded hover:bg-blue-600"
+            className="bg-green-500 text-white px-8 py-4 rounded hover:bg-green-600"
           >
             Vegetarian
           </button>
           <button 
             onClick={() => handleMenuTypeSelect('nonveg')}
-            className="bg-blue-500 text-white px-8 py-4 rounded hover:bg-blue-600"
+            className="bg-red-500 text-white px-8 py-4 rounded hover:bg-red-600"
           >
             Non-Vegetarian
           </button>
