f35ab6e3bef2079f1b1f601c8aca312f96310d9d
webhostdevs <webhostdev59@gmail.com>
2024-11-27 02:17:29 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -268,7 +268,7 @@ const Homepage = () => {
 </section>
 
 {/* Form Modal */}
-{/*             {showForm && (
+          {showForm && ({/*   
               <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
                 <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md relative">
                   <button
