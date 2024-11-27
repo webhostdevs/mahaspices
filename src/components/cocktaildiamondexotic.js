d1992afefc06b7799eaa3dbb5e6b7eee83cde44d
webhostdevs <webhostdev59@gmail.com>
2024-11-28 03:46:07 +0530

    Create cocktaildiamondexotic.js

---
+++ src/components/cocktaildiamondexotic.js
@@ -0,0 +1,130 @@
+const menuSections = {
+  snacks: {
+    title: 'SNACKS @ COCKTAIL SERVICE',
+    limit: 6,
+    items: [
+      'Green Salad',
+      'Sweet American Corn',
+      'Green Peas Masala',
+      'Cheese Balls',
+      'Cheese Potato Wedges Melt',
+      'Cheese Tart',
+      'Cocktail corn samosa',
+      'Corn Cheese Kabab',
+      'Corn Fitters',
+      'Corn Palak Roll',
+      'Crunchy Baby Corn',
+      'Cut Mirchi',
+      'Gold Coin',
+      'Golden Fried Baby Corn',
+      'Hara bhara Kabab',
+      'Masala Potato Fingers',
+      'Paneer Kesar Tikka',
+      'Paneer Shashlik',
+      'Potato Croquettes',
+      'Hangai Rolls',
+      'Smile Hearts',
+      'Veg. Corn Rolls',
+      'Veg. Seekh Kabab',
+      'Veg. Shashlik',
+      'Veg. Tart',
+      'Garden Spring Green Salad',
+      'Boiled Nuts',
+      'Sprouts',
+      'Cucumber Honey Yogurt Dill Salad'
+    ]
+  },
+  dipsAndSpreads: {
+    title: 'DIPS & SPREADS',
+    limit: 2,
+    items: [
+      'Mint Chutney',
+      'Hot Garlic Sauce',
+      'Garlic Mayo',
+      'Tomato Ketchup'
+    ]
+  },
+  beverages: {
+    title: 'BEVERAGES (ANY ONE)',
+    limit: 1,
+    items: [
+      'Water Melon',
+      'Pineapple',
+      'Musk Melon',
+      'Sugar Cane (Min. Gathering)',
+      'Coconut Delight',
+      'Fruit Punch',
+      'Mango Masti',
+      'Litchi Punch',
+      'Grapes Pulpy Kulfi',
+      'Orange Punch',
+      'Soft Drinks'
+    ]
+  },
+  salad: {
+    title: 'SALAD',
+    limit: 1,
+    items: [
+      'Garden Spring Green Salad',
+      'Boiled Nuts',
+      'Pineapple'
+    ]
+  },
+  sweets: {
+    title: 'SWEETS (SELECT ANY TWO)',
+    limit: 2,
+    items: [
+      'Badam Halwa',
+      'Mixed Fruit Halwa',
+      'Double Ka Meetha',
+      'Carrot Green Mixed Halwa',
+      'Qhubani Ka Meetha',
+      'Junnu',
+      'Pistabhog',
+      'Dry Chum Chum'
+    ]
+  },
+  liveSweets: {
+    title: 'LIVE SWEETS',
+    limit: 3,
+    items: [
+      'Honey Belam Jalebi',
+      'Pineapple Jalebi',
+      'Tawa Mix Sweet',
+      'American Mal Puri',
+      'Baked Rasgulla',
+      'Gajrela',
+      'Basundi'
+    ]
+  },
+  hotItems: {
+    title: 'HOTS (ANY ONE)',
+    limit: 1,
+    items: [
+      'Gaare',
+      'Mini Alsanda Wada',
+      'Nellore Chitti Gaare',
+      'Madras Pakodi (Round)',
+      'Pudina Masala Wada',
+      'Mealmaker Keema Wada',
+      'Veg. Lolly Pop',
+      'Mini Perugu Aawada',
+      'Vamu Mirchi Bajji'
+    ]
+  },
+  vegFries: {
+    title: 'VEG FRIES (ANY TWO)',
+    limit: 2,
+    items: [
+      'Aloo Capsicum Masala',
+      'Aloo Gobi',
+      'Aloo Jeera',
+      'Aloo Methi',
+      'Aloo, Allam, Green Mirchi Fry',
+      'Baby Corn Aloo Ullibonda, Fry'
+    ]
+  }
+};
+
+
+export { menuSections};
