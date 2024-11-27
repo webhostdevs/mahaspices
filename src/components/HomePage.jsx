918746f52a22aec39ee347f750d8644108ce70b1
webhostdevs <webhostdev59@gmail.com>
2024-11-28 02:59:34 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from 'framer-motion';
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom'; 
+import RotatingHeadings from './RotatingHeadings';
 import {
   Utensils,
   ChefHat,
