2f825b51c7d7ff3b92d458f3ca2b81dc6a51e79b
webhostdevs <webhostdev59@gmail.com>
2024-12-03 01:51:14 +0530

    Update MealBox.jsx

--- src/components/MealBox.jsx
+++ src/components/MealBox.jsx
@@ -318,6 +318,7 @@ const MealBox = () => {
                   </span>
                 </div>
           </div>
+          </div>
 
           <h2 className="text-2xl font-bold mb-6">Select Package Type</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
