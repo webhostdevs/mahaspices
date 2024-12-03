7a0e08ef7b12066b1eafb1be324c00190d7d737e
webhostdevs <webhostdev59@gmail.com>
2024-12-03 10:49:00 +0530

    Update MealBox.jsx

--- src/components/MealBox.jsx
+++ src/components/MealBox.jsx
@@ -697,7 +697,7 @@ const MealBox = () => {
 
       {/* Cart Summary */}
       {Object.keys(cart).length > 0 && (
-        <div className="fixed top-55 right-30 z-10 bg-white shadow-lg rounded-lg p-5 min-w-[204px]>
+        <div className="fixed top-55 right-30 z-10 bg-white shadow-lg rounded-lg p-5 min-w-[204px]">
           <h3 className="font-bold mb-2">Cart Summary</h3>
 
           {/* Group cart items by package */}
